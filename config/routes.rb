45c855f61e2cfe10b47c0221fbaa5aaf47317763
khadija swaleh <khadija12345@hotmail.co.uk>
2023-08-04 04:53:24 +0100

    routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# require "rails_helper"
+
+# begin
+#   require "lists_controller"
+# rescue LoadError
+# end
+resources :lists
+resources :movies
+resources :bookmarks
 end
